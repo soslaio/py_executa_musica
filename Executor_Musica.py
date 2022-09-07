91e94c7bf36837a7d7646b30926b3f37b835c094
Erica Santos Oliveira da Silva <erica.sos.adv@hotmail.com>
2022-09-07 13:10:25 -0300

    Inseri o codigo original

--- Executor_Musica.py
+++ Executor_Musica.py
@@ -1,2 +1,4 @@
-print('Hello, Mundo!')
-print('Ou não.')
+#copiar e colar o arquivo de música no Python. Clica com botão direito em cima do nome do projeto e seleciona paste.
+from playsound import playsound
+playsound('Musica.mp3')
+print('Tocando Musica.mp3')
