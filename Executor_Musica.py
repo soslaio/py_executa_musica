20beaddbe85d854ed6592f6cdb8248b52c5bcd6b
Rogério Pereira <rogerio.pereira@luizalabs.com>
2022-09-07 13:21:14 -0300

    ler o nome da música

--- Executor_Musica.py
+++ Executor_Musica.py
@@ -1,4 +1,5 @@
 #copiar e colar o arquivo de música no Python. Clica com botão direito em cima do nome do projeto e seleciona paste.
 from playsound import playsound
-playsound('Musica.mp3')
+nome_da_musica = input('Digite o nome da música: ')
+playsound(nome_da_musica)
 print('Tocando Musica.mp3')
